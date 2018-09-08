1fc095b638eba8d1ee118ae0d6684aad21ef4ace
David Mougeolle <david.mougeolle@moodule.net>
2018-09-08 09:11:32 +0200

    deploy & backup modes ; added cli args

--- setup.sh
+++ setup.sh
@@ -1,51 +1,85 @@
+#!/bin/bash
+
+# Globals #
+###########
+DATE=`date +%Y-%m-%d`
+PROFILE=${1:-"core"}
+MODE=${2:-"setup"}
+GCLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
 # Configure Linux #
 ###################
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-# Deploy Config Repo #
-#########################
-if [ -d ~/.dotfiles/ ]; then
-    rm -r ~/.dotfiles/
+# Config Repo #
+###############
+if [[ "$MODE" = *"setup"* ]]; then
+    cp -f -r ./dotfiles/ ~/.dotfiles/
+elif [[ "$MODE" = *"backup"* ]]; then
+    cp -f -r ~/.dotfiles/ ./dotfiles/
 fi
-cp -r ./dotfiles/ ~/.dotfiles/
-#cd ~/.dotfiles/
 
 # Link Configuration to Repo #
 ##############################
 
 # Sublime Text #
 ################
-wget -q -O - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+if [[ "$MODE" = *"setup"* ]]; then
+    wget -q -O - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+fi
 
 # Brave Browser #
 #################
-# wget https://s3-us-west-2.amazonaws.com/brave-apt/keys.asc | sudo apt-key add -
-# echo "deb [arch=amd64] https://s3-us-west-2.amazonaws.com/brave-apt `lsb_release -sc` main" | sudo tee -a /etc/apt/sources.list.d/brave-browser.list
+# if [[ "$MODE" = *"setup"* ]]; then
+#     wget https://s3-us-west-2.amazonaws.com/brave-apt/keys.asc | sudo apt-key add -
+#     echo "deb [arch=amd64] https://s3-us-west-2.amazonaws.com/brave-apt `lsb_release -sc` main" | sudo tee -a /etc/apt/sources.list.d/brave-browser.list
+# fi
 
 # Google Chrome #
 #################
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# if [[ "$MODE" = *"setup"* ]]; then
+#     wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#     echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# fi
 
 # Google Cloud #
 ################
-GCLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb http://packages.cloud.google.com/apt $GCLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+if [[ "$MODE" = *"setup"* ]]; then
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    echo "deb http://packages.cloud.google.com/apt $GCLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+fi
 
 # Install apt packages #
 ########################
-sudo apt update
-sudo apt-get install $(grep -vE "^\s*#" dists/profiles/core.packages  | tr "\n" " ")
-sudo apt-get install $(grep -vE "^\s*#" dists/profiles/tools.packages  | tr "\n" " ")
-#sudo apt-get install $(grep -vE "^\s*#" dists/profiles/dev.packages  | tr "\n" " ")
-sudo apt-get install $(grep -vE "^\s*#" dists/profiles/media.packages  | tr "\n" " ")
+if [[ "$MODE" = *"setup"* ]]; then
+    sudo apt update
+    sudo apt-get install $(grep -vE "^\s*#" dists/profiles/core.packages  | tr "\n" " ")
+    if [[ "$PROFILE" = *"tools"* ]]; then sudo apt-get install $(grep -vE "^\s*#" dists/profiles/tools.packages  | tr "\n" " "); fi
+    if [[ "$PROFILE" = *"dev"* ]]; then sudo apt-get install $(grep -vE "^\s*#" dists/profiles/dev.packages  | tr "\n" " "); fi
+    if [[ "$PROFILE" = *"media"* ]]; then sudo apt-get install $(grep -vE "^\s*#" dists/profiles/media.packages  | tr "\n" " "); fi
+fi
 
 # Install pip packages #
 ########################
 
 # Install snap packages #
 #########################
-#sudo snap install heroku --classic
+if [[ "$MODE" = *"setup"* ]]; then
+    if [[ "$PROFILE" = *"dev"* ]]; then sudo snap install heroku --classic; fi
+fi
+
+# Stow the dotfiles #
+#####################
+if [[ "$MODE" = *"setup"* ]]; then
+    for filename in ~/.dotfiles/*/ ~/.dotfiles/.*/ ; do
+        stow $filename
+    done
+fi
+
+# Push the changes to github #
+##############################
+if [[ "$MODE" = *"backup"* ]]; then
+    git commit -a -m "config file update ($DATE)"
+fi
