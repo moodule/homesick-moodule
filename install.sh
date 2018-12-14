87ca0209917b8d4244540c26cf32c67692292a89
David Mougeolle <david.mougeolle@moodule.net>
2018-12-15 00:25:06 +0100

    install packages & dotfiles

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Globals #
+###########
+DATE=`date +%Y-%m-%d`
+PROFILE=${1:-"core"}
+GCLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Configure Linux #
+###################
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+# Pull the dotfiles #
+#####################
+if [[ "$PROFILE" = *"dot"* ]]; then
+    git pull
+    rsync -avh --progress --inplace ./dotfiles/ ~/.dotfiles/
+    if [ -d ~/.dotfiles/bin/bin/ ]; then
+        for filename in ~/.dotfiles/bin/bin/* ; do
+            chmod a+x $filename
+        done
+    fi
+fi
+
+# Stow the dotfiles #
+#####################
+if [[ "$PROFILE" = *"dot"* ]]; then
+    for filename in ~/.dotfiles/*/ ; do
+        stow $filename
+    done
+fi
+
+# Sublime Text #
+################
+wget -q -O - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# Brave Browser #
+#################
+# wget https://s3-us-west-2.amazonaws.com/brave-apt/keys.asc | sudo apt-key add -
+# echo "deb [arch=amd64] https://s3-us-west-2.amazonaws.com/brave-apt `lsb_release -sc` main" | sudo tee -a /etc/apt/sources.list.d/brave-browser.list
+
+# Google Chrome #
+#################
+# wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+# echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+
+# Google Cloud #
+################
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://packages.cloud.google.com/apt $GCLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Install apt packages #
+########################
+sudo apt update
+if [[ "$PROFILE" = *"core"* ]]; then sudo apt-get install $(grep -vE "^\s*#" dists/profiles/core.packages  | tr "\n" " "); fi
+if [[ "$PROFILE" = *"tools"* ]]; then sudo apt-get install $(grep -vE "^\s*#" dists/profiles/tools.packages  | tr "\n" " "); fi
+if [[ "$PROFILE" = *"dev"* ]]; then sudo apt-get install $(grep -vE "^\s*#" dists/profiles/dev.packages  | tr "\n" " "); fi
+if [[ "$PROFILE" = *"media"* ]]; then sudo apt-get install $(grep -vE "^\s*#" dists/profiles/media.packages  | tr "\n" " "); fi
+
+# Install pip packages #
+########################
+
+# Install snap packages #
+#########################
+if [[ "$PROFILE" = *"dev"* ]]; then sudo snap install heroku --classic; fi
