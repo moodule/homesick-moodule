3c7dbc806f698db71f3a267a7c82976381c86995
David Mougeolle <david.mougeolle@moodule.net>
2019-04-23 09:22:38 +0200

    Add docker

--- install.sh
+++ install.sh
@@ -39,6 +39,13 @@ fi
 wget -q -O - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
+# Docker #
+##########
+if [[ "$PROFILE" = *"dev"* ]]; then
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+fi
+
 # Brave Browser #
 #################
 # wget https://s3-us-west-2.amazonaws.com/brave-apt/keys.asc | sudo apt-key add -
