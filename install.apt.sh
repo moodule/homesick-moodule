2ba19a8f88b452b30c45f86d0cc67efce7f63e45
David Mougeolle <moodule@protonmail.com>
2020-01-08 10:57:07 +0100

    Install mongodb only in 'dev' profile

--- install.apt.sh
+++ install.apt.sh
@@ -55,7 +55,7 @@ fi
 
 # MONGO DB #
 ############
-if [[]]; then
+if [[ "$PROFILE" = *"dev" ]]; then
     wget -q -O - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
     echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org.list
 fi
