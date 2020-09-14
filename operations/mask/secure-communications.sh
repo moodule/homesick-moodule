a2600ac379934d891b2c601113861670fe972d4b
moodule <moodule@protonmail.com>
2020-09-14 17:03:46 +0000

    List darknet mail providers

---
+++ operations/mask/secure-communications.sh
@@ -0,0 +1,14 @@
+# mail
+# guerrillamail - http://grrmailb3fxpjbwm.onion/
+# guerrillamail - https://www.guerrillamail.com/
+# temp mail - https://tempmailaddress.com
+# torbox - http://torbox3uiot6wchz.onion/
+# elude - http://eludemaillhqfkh5.onion/
+# Riseup - http://nzh3fv6jc6jskki3.onion
+# mail2tor - http://mail2tor2zyjdctd.onion/
+# list - https://gist.github.com/michenriksen/8710649
+# list of providers - https://www.reddit.com/r/onions/comments/6krt34/list_of_onion_email_providers/
+
+# xmpp
+# list of servers - https://gist.github.com/dllud/a46d4a555e31dfeff6ad41dcf20729ac
+# pidgin - xmpp client
