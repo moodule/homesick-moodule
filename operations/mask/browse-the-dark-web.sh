0fddb8e83ce42e523e26b0e61450814e7cd7b39d
moodule <moodule@protonmail.com>
2020-09-14 15:08:05 +0000

    List some entry points

---
+++ operations/mask/browse-the-dark-web.sh
@@ -0,0 +1,8 @@
+# hidden wiki http://zqktlwi4fecvo6ri.onion/wiki/index.php/Main_Page
+# dark.fail darkfailllnkf4vf.onion - PGP verifies links
+# deep web subredit: https://www.reddit.com/r/deepweb/
+# onions subreddit - https://www.reddit.com/r/onions/
+
+# hidden answers answerstedhctbek.onion
+# dread - reddit http://dreadditevelidot.onion/
+# privacy sub reddit - https://www.reddit.com/r/privacy/
