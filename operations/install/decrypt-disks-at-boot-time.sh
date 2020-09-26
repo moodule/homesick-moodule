c0862e0e94ac95df1008a4b302e0e81949ed1392
moodule <moodule@protonmail.com>
2020-09-26 14:02:05 +0200

    Add missing modules & hooks to load raid disks prior to luks on boot

--- operations/install/decrypt-disks-at-boot-time.sh
+++ operations/install/decrypt-disks-at-boot-time.sh
@@ -5,4 +5,4 @@ echo "CRYPTSETUP=y" >> /etc/cryptsetup-initramfs/conf-hook
 echo "md_crypt UUID=11fdb53b-d45c-4b46-b946-b67de3b448f5 none luks,discard" >> /etc/crypttab
 
 # /etc/grub/default
-GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=UUID=11fdb53b-d45c-4b46-b946-b67de3b448f5:md0_crypt root=/dev/mapper/stable-root resume=UUID=106e8c63-91b2-4c8a-aeff-5c14ea1dfea2"
+GRUB_CMDLINE_LINUX_DEFAULT="cryptdevice=UUID=11fdb53b-d45c-4b46-b946-b67de3b448f5:md_crypt root=UUID=b983db89-6be4-45aa-9745-b33f4dff90c5 resume=UUID=106e8c63-91b2-4c8a-aeff-5c14ea1dfea2"
--- operations/install/load-lvm-raid-modules-at-boot-time.sh
+++ operations/install/load-lvm-raid-modules-at-boot-time.sh
@@ -1,2 +1,9 @@
-# /etc/default/grub
+mdadm --detail --scan >> /etc/mdadm.conf
+
+# (/target)/etc/default/grub
 GRUB_PRELOAD_MODULES="part_gpt part_msdos ntfs mdraid09 mdraid1x lvm"
+
+# (/target)/etc/mkinitcpio.conf ramfs hooks
+MODULES=(dm-raid raid0 raid1 raid10 raid456 usbhid xhci_hcd)
+FILES=(/crypto_keyfile.bin)
+HOOKS=(base udev autodetect keyboard keymap consolefont modconf block mdadm_udev lvm2 encrypt filesystems fsck)
