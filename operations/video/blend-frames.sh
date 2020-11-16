42ea12ad95d884590fa3aaf85bf71a747413b522
moodule <moodule@protonmail.com>
2020-11-16 12:30:25 +0000

    Blend any number of frames at once, instead of just 2

--- operations/video/blend-frames.sh
+++ operations/video/blend-frames.sh
@@ -5,3 +5,6 @@ ffmpeg -i input.mp4 -vf "tblend=average,framestep=2" -r 30 output.mp4
 
 # this can be iterated: 4 frames are averaged
 ffmpeg -i input.mp4 -vf "tblend=average,framerate=2,tblend=average,framerate=2" -r 30 output.mp4
+
+# any number of frames at once
+ffmpeg -i input.mp4 -vf "tmix=frames=4:weights='1 1 1 1'" output.mp4
