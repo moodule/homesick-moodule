8f0952ea3dafdcd607d42a73798ae69415e9add3
moodule <moodule@protonmail.com>
2021-01-13 13:16:19 +0000

    Parametrize the script for direct use

--- operations/protect/backup-your-files.sh
+++ operations/protect/backup-your-files.sh
@@ -1,10 +1,28 @@
+# encrypt using the key associated with
+OWNER=moodule@protonmail.com
+
+# the file / folder to backup
+TARGET=$(realpath ~/documents/)
+PARENT=$(dirname "$TARGET")
+NAME=$(basename "$TARGET")
+
+# final resting place for the data
+BACKUP=$(realpath /mnt/backup/)
+
+# put the encrypted files in a temp dir
+CACHE=$(mktemp -d)
+
+# gpgtar doesn't work outside of the cwd
+cd "$PARENT"
+
 # encrypt and compress a folder
 gpgtar --verbose --encrypt \
-    --recipient moodule@protonmail.com \
-    --output /tmp/backup/documents.gpg \
-    ~/documents
+    --recipient "$OWNER" \
+    --output "$CACHE"/"$NAME".gpg \
+    "$NAME"
 
 # copy the archives
-rsync -avh --progress --update \
-    /tmp/backup/ \
-    /mnt/usb/backup/
+sudo rsync -avh --progress --update "$CACHE"/ "$BACKUP"/
+
+# clean up
+rm -rf "$CACHE"/
