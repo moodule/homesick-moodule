3d60e18f69f79316dfd60bba6ca0ebfc577177a9
moodule <moodule@protonmail.com>
2021-03-02 20:50:27 +0100

    Keep styling when piped to a file, toggle file changes

--- operations/devops/visualize-git-history.sh
+++ operations/devops/visualize-git-history.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
 # display branch history in a text tree
 git log --graph --full-history --all --pretty=format:"%h%x09%d%x20%s"
+
+# output ANSI codes for the colors, even when piping to a file
+git log --color=always
+
+# display the actual diff
+git log -p
